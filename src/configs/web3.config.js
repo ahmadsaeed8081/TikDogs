74e12788a250a0177b8216831bdb487fd21a165c
ahmad <ahmadsaeed8081@gmail.com>
2024-11-15 15:37:25 +0500

    config updtaed

--- src/configs/web3.config.js
+++ src/configs/web3.config.js
@@ -7,10 +7,10 @@ import { createClient } from 'viem'
 
 const projectId = process.env.REACT_APP_WC_PROJECT_ID;
 const metadata = {  
-    name: "YouSamaritan",
+    name: "Tikdogs",
     description: "",
-    url: "https://yousamaritan.com/",
-    icons: ["https://yousamaritan.com/"]
+    url: "https://Tikdogs.com/",
+    icons: ["https://Tikdogs.com/"]
     
 };
 
